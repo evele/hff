0dfe2de42123145680799ecbc461ecc87141546b
Manuel Salvador <manu.sacr@hotmail.com>
2024-08-04 12:12:22 -0300

    Update use-create-client.ts

--- front/src/hooks/use-create-client.ts
+++ front/src/hooks/use-create-client.ts
@@ -1,18 +1,28 @@
 import { useEffect, useState } from 'react';
-import { createPublicClient, createWalletClient, custom, http, WalletClient } from 'viem';
+import {
+  createPublicClient,
+  createWalletClient,
+  custom,
+  http,
+  PublicClient,
+  WalletClient,
+} from 'viem';
 import { scrollSepolia } from 'viem/chains';
 import 'viem/window';
 
 export default function useCreateClient() {
   const [walletClient, setWalletClient] = useState<null | WalletClient>(null);
-
-  const publicClient = createPublicClient({
-    chain: scrollSepolia,
-    transport: http('https://sepolia-rpc.scroll.io'),
-  });
+  const [publicClient, setPublicClient] = useState<null | PublicClient>(null);
 
   useEffect(() => {
     if (window && window.ethereum) {
+      const publicClient = createPublicClient({
+        chain: scrollSepolia,
+        transport: http('https://sepolia-rpc.scroll.io'),
+      });
+
+      setPublicClient(publicClient);
+
       const walletClient = createWalletClient({
         chain: scrollSepolia,
         transport: custom(window.ethereum),
